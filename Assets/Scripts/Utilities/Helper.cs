b5f9b80fdb49878bc482e40353e2a2d7c888cb2f
Miguel Lopes <miguel.lopes@king.com>
2021-12-30 13:46:08 +0100

    Finish right hand (single and two handed) animations

--- Assets/Scripts/Utilities/Helper.cs
+++ Assets/Scripts/Utilities/Helper.cs
@@ -5,6 +5,15 @@ namespace Utilities
     public class Helper : MonoBehaviour
     {
         [Range(0, 1)] [SerializeField] private float vertical;
+
+
+        [SerializeField] private string animName;
+        [SerializeField] private bool playAnim;
+        [SerializeField] private bool twoHanded;
+        [SerializeField] private bool enableRootMotion;
+        
+        [SerializeField]private string[] oh_attacks;
+        [SerializeField]private string[] th_attacks;
         private Animator anim;
         // Start is called before the first frame update
         void Start()
@@ -15,6 +24,35 @@ namespace Utilities
         // Update is called once per frame
         void Update()
         {
+            enableRootMotion = !anim.GetBool("canMove");
+            anim.applyRootMotion = enableRootMotion;
+
+            if (enableRootMotion)
+            {
+                return;
+            }
+            
+            anim.SetBool("two_handed", twoHanded);
+            if (playAnim)
+            {
+                string targetAnim;
+                if (!twoHanded)
+                {
+                    int r = Random.Range(0, oh_attacks.Length);
+                    targetAnim = oh_attacks[r];
+                }
+                else
+                {
+                    int r = Random.Range(0, th_attacks.Length);
+                    targetAnim = th_attacks[r];
+                }
+                vertical = 0;
+                
+                anim.CrossFade(targetAnim, 0.2f);
+                /*anim.SetBool("canMove", false);
+                enableRootMotion = true;*/
+                playAnim = false;
+            }
             anim.SetFloat("vertical", vertical);
         }
     }
